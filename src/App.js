cf35e8252321fdbebcc99a9214142309354274a2
Matthias Hölzl <tc@xantira.com>
2019-08-25 22:56:12 +0200

    A fresh start for training purposes.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,11 @@
 import React from 'react';
-import HeroBar from './HeroBar';
-import NumberInputSegment from './NumberInputSegment';
-
-import PrimeSegment from './PrimeSegment';
-import factorize from './factorize';
 
 /**
  * The main entry point for the application
  */
 class App extends React.Component {
-  state = { numbers: [] };
-
-  /**
-   * Adds the value of `stringOrNumber` to `this.state.numbers` if it can
-   * be converted to an integer. Otherwise leaves the state unmodified.
-   * @param {string | number} stringOrNumber - Representation of the number to add
-   */
-  addNumber = stringOrNumber => {
-    const number = isNaN(stringOrNumber)
-      ? parseInt(stringOrNumber)
-      : stringOrNumber;
-    if (!isNaN(number)) {
-      this.setState({ numbers: this.state.numbers.concat([number]) });
-    }
-  };
-
   render(props) {
-    return (
-      <div className='ui container text'>
-        <HeroBar icon='cogs'>Prime Factorization</HeroBar>
-        <NumberInputSegment
-          labelText='Please type a number to be factorized.'
-          onSubmit={this.addNumber}
-        />
-        {this.state.numbers.map((number, index) => (
-          <PrimeSegment
-            key={index}
-            number={number}
-            primes={factorize(number)}
-          />
-        ))}
-      </div>
-    );
+    return <div className='ui container text'>Prime Factors</div>;
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -2,87 +2,9 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 
 import App from './App';
-import HeroBar from './HeroBar';
-import NumberInputSegment from './NumberInputSegment';
-import PrimeSegment from './PrimeSegment';
 
 describe('<App />', () => {
   it('renders without crashing', () => {
     shallow(<App />);
-  });
-
-  it('contains HeroBar', () => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper).toContainMatchingElement('HeroBar');
-  });
-
-  it('contains NumberInputSegment', () => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper).toContainMatchingElement('NumberInputSegment');
-  });
-
-  it('initially contains no PrimeSegment', () => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper).not.toContainMatchingElement('PrimeSegment');
-  });
-
-  it('contains PrimdSegment after adding number', () => {
-    const wrapper = shallow(<App />);
-    wrapper.instance().addNumber(42);
-    expect(wrapper).toContainMatchingElement('PrimeSegment');
-  });
-});
-
-describe('<App /> (different testing strategy)', () => {
-  it('is a div', () => {
-    expect(shallow(<App />).type()).toBe('div');
-  });
-
-  it('has classes `ui`, `container` and `text`.', () => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper).toHaveClassName('ui container text');
-  });
-
-  it('has two children', () => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper.children().length).toBe(2);
-  });
-
-  it('has HeroBar as first child', () => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper.childAt(0).type()).toBe(HeroBar);
-  });
-
-  it('has NumberInputSegment as second child', () => {
-    const wrapper = shallow(<App />);
-
-    expect(wrapper.childAt(1).type()).toBe(NumberInputSegment);
-  });
-
-  it('has three children after adding a number', () => {
-    const wrapper = shallow(<App />);
-    wrapper.instance().addNumber(42);
-
-    expect(wrapper.children().length).toBe(3);
-  });
-
-  it('has PrimeSegment as third child', () => {
-    const wrapper = shallow(<App />);
-    wrapper.instance().addNumber(42);
-
-    expect(wrapper.childAt(2).type()).toBe(PrimeSegment);
-  });
-});
-
-describe('<App /> (snapshot test)', () => {
-  it('matches snapshot', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper).toMatchSnapshot();
   });
 });
